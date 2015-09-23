1e4090b3ea1ddd2e7dd3ebab379b94621de5d322
Bob Ballance <ballance@swcp.com>
2015-09-23 09:28:33 -0600

    Cross compile working with minor fudges.

--- Makefile
+++ Makefile
@@ -1,6 +1,19 @@
-# Generiic simple makefile
+#
+# For cross-compiling --- we need to set up some different paths.
+#
+# What this makefile does *not* do is to change the .mk include paths. You have to do that from an environment
+# variable (MAKESYSPATH) or the -m flag for the make command.
+#
+
+
+KERNCONF = RPI2_DEV
+XCOMPILE_TARGET=arm.armv6
+CROCHET_OBJ=$(CROCHET_DIR)/work/obj/$(XCOMPILE_TARGET)/$(FREEBSD_SRC)/sys/$(KERNCONF)
 
 KMOD = gpio_pps
 SRCS = gpio_pps.c
 
+CFLAGS+= -I$(FREEBSD_SRC)/sys  -I$(CROCHET_OBJ)
+
 .include <bsd.kmod.mk>
+
--- gpio_pps.c
+++ gpio_pps.c
@@ -1,9 +1,58 @@
-/* Start by creating just a hello-world-type module */
+#include <sys/cdefs.h>
 
+#include "opt_platform.h"
 #include <sys/param.h>
-#include <sys/module.h>
-#include <sys/kernel.h>
+
+/* Temporary hack? */
+/* Clang-style message */
+#pragma message "Overriding the FREEBSD Version Number is a Bad Idea!"
+
+#undef __FreeBSD_version
+#define __FreeBSD_version 1100075	/* Master, propagated to newvers */
+
+
 #include <sys/systm.h>
+#include <sys/bus.h>
+#include <sys/gpio.h>
+#include <sys/kernel.h>
+#include <sys/lock.h>
+#include <sys/malloc.h>
+#include <sys/module.h>
+#include <sys/mutex.h>
+#include <sys/sysctl.h>
+
+#include <sys/timepps.h>
+
+#include <dev/fdt/fdt_common.h>
+ #include <dev/ofw/ofw_bus.h>
+
+#include <dev/gpio/gpiobusvar.h>
+
+#include "gpiobus_if.h"
+
+
+#define GPIOBL_LOCK(_sc)		mtx_lock(&(_sc)->sc_mtx)
+#define	GPIOBL_UNLOCK(_sc)		mtx_unlock(&(_sc)->sc_mtx)
+#define GPIOBL_LOCK_INIT(_sc) \
+	mtx_init(&_sc->sc_mtx, device_get_nameunit(_sc->sc_dev), \
+	    "gpiobacklight", MTX_DEF)
+#define GPIOBL_LOCK_DESTROY(_sc)	mtx_destroy(&_sc->sc_mtx);
+
+struct gpio_pps_softc 
+{
+	device_t	sc_dev;
+	device_t	sc_busdev;
+	struct mtx	sc_mtx;
+
+	struct sysctl_oid	*sc_oid;
+        /* sio.c needs a single pps_state */
+  	struct	pps_state pps;
+
+       /* these from parallel bus. Don't know if they are needed yet. */
+	struct resource *intr_resource;	/* interrupt resource */
+	void *intr_cookie;		/* interrupt registration cookie */
+
+};
 
 static int
 gpio_pps_modevent(module_t mod __unused, int event, void* arg __unused)
